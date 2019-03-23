caf573c0c45b0d191a62bc22e05d43dc47704c66
Aman <amangram@github.com>
2019-03-23 08:47:09 +0600

    some changes

--- app/src/main/java/com/amangram/tablayout/FirstFragment.kt
+++ app/src/main/java/com/amangram/tablayout/FirstFragment.kt
@@ -3,9 +3,11 @@ package com.amangram.tablayout
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_first.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -16,8 +18,23 @@ private const val ARG_PARAM2 = "param2"
 /**
  * A simple [Fragment] subclass.
  *
- */
+ */data class Movie(val title: String, val year: Int)
 class FirstFragment : Fragment() {
+    private val mNicolasCageMovies = listOf(
+        Movie("Raising Arizona", 1987),
+        Movie("Vampire's Kiss", 1988),
+        Movie("Con Air", 1997),
+        Movie("Gone in 60 Seconds", 1997),
+        Movie("National Treasure", 2004),
+        Movie("The Wicker Man", 2006),
+        Movie("Ghost Rider", 2007),
+        Movie("Knowing", 2009)
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +43,23 @@ class FirstFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // RecyclerView node initialized here
+        list_recycler_view.apply {
+            // set a LinearLayoutManager to handle Android
+            // RecyclerView behavior
+            layoutManager = LinearLayoutManager(activity)
+            // set the custom adapter to the RecyclerView
+            adapter = ListAdapter(mNicolasCageMovies)
+        }
+    }
 
+    companion object {
+        fun newInstance(): FirstFragment = FirstFragment()
 
+    }
 }
+
+
+
